6ac5317119d3eb03a6dde6b95739572f3f332e5a
Viktoriia-Dasci <74453524+Viktoriia-Dasci@users.noreply.github.com>
2023-07-14 23:38:08 +0200

    Create explainabilty_functions.py

---
+++ explainabilty_functions.py
@@ -0,0 +1,176 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
+import seaborn as sns
+from scipy.ndimage import zoom
+from skimage.color import rgb2gray
+from lime import lime_image
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from skimage.util import img_as_float
+import PIL
+import saliency
+
+
+#Grad-CAM & Guided Grad-CAM
+@tf.custom_gradient
+def guidedRelu(x):
+    def grad(dy):
+        return tf.cast(dy>0,"float32") * tf.cast(x>0, "float32") * dy
+    return tf.nn.relu(x), grad
+
+
+class GuidedBackprop:
+    def __init__(self,model, layerName=None):
+        self.model = model
+        self.layerName = layerName
+        if self.layerName == None:
+            self.layerName = self.find_target_layer()
+        self.gbModel = self.build_guided_model()
+
+    def find_target_layer(self):
+        for layer in reversed(self.model.layers):
+            if len(layer.output_shape) == 4:
+                return layer.name
+        raise ValueError("Could not find 4D layer. Cannot apply Guided Backpropagation")
+
+    def build_guided_model(self):
+        gbModel = Model(inputs=[self.model.inputs], outputs=[self.model.get_layer(self.layerName).output])
+        layer_dict = [layer for layer in gbModel.layers[1:] if hasattr(layer,"activation")]
+        for layer in layer_dict:
+            if layer.activation == tf.keras.activations.relu:
+                layer.activation = guidedRelu
+        return gbModel
+
+    def guided_backprop(self, images, upsample_size):
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(images, tf.float32)
+            tape.watch(inputs)
+            outputs = self.gbModel(inputs)
+        grads = tape.gradient(outputs, inputs)[0]
+        saliency = cv2.resize(np.asarray(grads), upsample_size)
+        return saliency
+
+
+def deprocess_image(x):
+    x = x.copy()
+    x -= x.mean()
+    x /= (x.std() + K.epsilon())
+    x *= 0.25
+    x += 0.5
+    x = np.clip(x, 0, 1)
+    x *= 255
+    if K.image_data_format() == 'channels_first':
+        x = x.transpose((1, 2, 0))
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+class GradCAM:
+    def __init__(self, model, layerName=None):
+        self.model = model
+        self.layerName = layerName
+        if self.layerName == None:
+            self.layerName = self.find_target_layer()
+
+    def find_target_layer(self):
+        for layer in reversed(self.model.layers):
+            if len(layer.output_shape) == 4:
+                return layer.name
+        raise ValueError("Could not find 4D layer. Cannot apply GradCAM")
+
+    def compute_heatmap(self, image, upsample_size, classIdx=None, eps=1e-5):
+        gradModel = Model(inputs=[self.model.inputs], outputs=[self.model.get_layer(self.layerName).output, self.model.output])
+        with tf.GradientTape() as tape:
+            inputs = tf.cast(image, tf.float32)
+            (convOuts, preds) = gradModel(inputs)
+            if classIdx is None:
+                classIdx = np.argmax(preds)
+            loss = preds[:, classIdx]
+        grads = tape.gradient(loss, convOuts)
+        convOuts = convOuts[0]
+        grads = grads[0]
+        norm_grads = tf.divide(grads, tf.reduce_mean(tf.square(grads)) + tf.constant(eps))
+        weights = tf.reduce_mean(norm_grads, axis=(0, 1))
+        cam = tf.reduce_sum(tf.multiply(weights, convOuts), axis=-1)
+        cam = np.maximum(cam, 0)
+        cam = cam / np.max(cam)
+        cam = cv2.resize(cam, upsample_size,cv2.INTER_LINEAR)
+        cam3 = np.expand_dims(cam, axis=2)
+        cam3 = np.tile(cam3, [1, 1, 3])
+        return cam3
+
+
+def overlay_gradCAM(img, cam3):
+    cam3 = np.uint8(255 * cam3)
+    cam3 = cv2.applyColorMap(cam3, cv2.COLORMAP_HOT)
+    new_img = 0.6 * cam3 + 0.2 * img
+    return (new_img * 255.0 / new_img.max()).astype("uint8")
+
+#LIME
+
+def explanation_heatmap(exp, exp_class):
+    dict_heatmap = dict(exp.local_exp[exp_class])
+    heatmap = np.vectorize(dict_heatmap.get)(exp.segments)
+    return heatmap
+
+
+def colorbar(mappable):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    import matplotlib.pyplot as plt
+    last_axes = plt.gca()
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(mappable, cax=cax)
+    plt.sca(last_axes)
+    return cbar
+
+#XRAI method
+
+def ShowImage(im, title='', ax=None):
+    if ax is None:
+        P.figure()
+    P.axis('off')
+    P.imshow(im)
+    P.title(title)
+
+def ShowGrayscaleImage(im, title='', ax=None):
+    if ax is None:
+        P.figure()
+    P.axis('off')
+    P.imshow(im, cmap=P.cm.gray, vmin=0, vmax=1)
+    P.title(title)
+
+def ShowHeatMap(im, title, ax=None):
+    if ax is None:
+        P.figure()
+    P.axis('off')
+    P.imshow(im, cmap='inferno')
+    P.title(title)
+
+def LoadImage(file_path):
+    im = PIL.Image.open(file_path)
+    im = im.resize((224,224))
+    im = np.asarray(im)
+    return im
+
+def call_model_function(images, call_model_args=None, expected_keys=None):
+    target_class_idx =  call_model_args[class_idx_str]
+    images = tf.convert_to_tensor(images)
+    with tf.GradientTape() as tape:
+        if expected_keys==[saliency.base.INPUT_OUTPUT_GRADIENTS]:
+            tape.watch(images)
+            _, output_layer = model(images)
+            output_layer = output_layer[:,target_class_idx]
+            gradients = np.array(tape.gradient(output_layer, images))
+            return {saliency.base.INPUT_OUTPUT_GRADIENTS: gradients}
+        else:
+            conv_layer, output_layer = model(images)
+            gradients = np.array(tape.gradient(output_layer, conv_layer))
+            return {saliency.base.CONVOLUTION_LAYER_VALUES: conv_layer,
+                    saliency.base.CONVOLUTION_OUTPUT_GRADIENTS: gradients}
